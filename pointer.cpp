006a02db73a911dd6e65e5143f388685b7f33489
yooseungyeon31 <ysy8317433@gmail.com>
2026-03-06 17:27:41 +0900

    포인터 관련 필기

--- pointer.cpp
+++ pointer.cpp
@@ -53,3 +53,46 @@ int main()
 //---------
 
 
+
+
+//======================
+
+#include <iostream>
+
+using namespace std;
+
+//call by value이용한 포인터 참조 연산
+void Swap(int* PA, int* PB)
+{
+	int Temp = *PA;
+	*PA = *PB;
+	*PB = Temp;
+}
+
+//C++ 참조자
+//call by reference
+//void Swap(int& PA, int& PB)
+//{
+//	int Temp = PA;
+//	PA = PB;
+//	PB = Temp;
+//}
+
+int main()
+{
+	int* PA = 0;
+	int A = 10;
+	int B = 20;
+
+	cout << &A << endl;
+	cout << &B << endl;
+
+
+	Swap(&A, &B);
+
+	cout << A << endl;
+	cout << B << endl;
+
+
+	return 0;
+}
